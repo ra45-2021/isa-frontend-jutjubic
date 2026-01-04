a667bffbd0c8eec225131b4c2803ffa767eebf46
Milica Savic <149960261+ra45-2021@users.noreply.github.com>
2026-01-04 21:17:36 +0100

    feat/Improved Home Page and NavBar

--- jutjubic-front/src/app/app.routes.ts
+++ jutjubic-front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { HomeComponent } from './layout/home/home.component';
-
+import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +9,7 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
   }
--- jutjubic-front/src/app/layout/home/home.component.ts
+++ jutjubic-front/src/app/layout/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 interface UserPublic {
   id: number;
@@ -12,6 +13,7 @@ interface PostFeed {
   id: number;
   title: string;
   description?: string | null;
+  tags?: string[];
   videoUrl: string;
   thumbnailUrl?: string | null;
   createdAt: string;
@@ -31,7 +33,13 @@ export class HomeComponent implements OnInit {
   loading = true;
   error = '';
 
-  constructor(private http: HttpClient) {}
+  // TODO: replace later with real auth
+  isLoggedIn = false;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.http.get<PostFeed[]>('/api/posts').subscribe({
@@ -44,5 +52,40 @@ export class HomeComponent implements OnInit {
         this.loading = false;
       }
     });
+  }
+
+  hasTags(p: PostFeed): boolean {
+    return Array.isArray(p.tags) && p.tags.length > 0;
+  }
+
+  actionGuard(): void {
+    if (!this.isLoggedIn) {
+      this.router.navigateByUrl('/login');
+      return;
+    }
+
+    console.log('Action allowed');
+  }
+
+  timeLabel(iso: string): string {
+    const d = new Date(iso);
+    const now = new Date();
+
+    const diffMs = now.getTime() - d.getTime();
+    const diffMin = Math.floor(diffMs / 60000);
+
+    if (diffMin < 1) return 'just now';
+    if (diffMin < 60) return `${diffMin}m ago`;
+
+    const diffH = Math.floor(diffMin / 60);
+    if (diffH < 24) return `${diffH}h ago`;
+
+    const diffD = Math.floor(diffH / 24);
+    if (diffD < 7) return `${diffD}d ago`;
+
+    const diffW = Math.floor(diffD / 7);
+    if (diffW < 4) return `${diffW}w ago`;
+
+    return d.toLocaleDateString();
   }
 }
